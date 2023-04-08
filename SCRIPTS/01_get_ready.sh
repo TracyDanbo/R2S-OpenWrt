697a74e97f23c7017740ac7ad5cce6e68845eb23
tracy <xingsuan@gmail.com>
2023-04-08 10:11:07 +0800

    udpdate script

--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -26,6 +26,7 @@ cp -rf ./openwrt_release/package/kernel/linux/* ./openwrt/package/kernel/linux/
 
 # 获取源代码
 git clone -b master --depth 1 https://github.com/immortalwrt/immortalwrt.git immortalwrt
+git clone -b openwrt-21.02 --depth 1 https://github.com/immortalwrt/immortalwrt.git immortalwrt_21
 git clone -b master --depth 1 https://github.com/immortalwrt/packages.git immortalwrt_pkg
 git clone -b master --depth 1 https://github.com/immortalwrt/luci.git immortalwrt_luci
 git clone -b master --depth 1 https://github.com/coolsnowwolf/lede.git lede
@@ -42,10 +43,12 @@ git clone -b packages --depth 1 https://github.com/xiaorouji/openwrt-passwall pa
 git clone -b luci --depth 1 https://github.com/xiaorouji/openwrt-passwall passwall_luci
 git clone -b main --depth 1 https://github.com/jjm2473/openwrt-third openwrt-third
 git clone -b master --depth 1 https://github.com/lisaac/luci-app-dockerman dockerman
+git clone -b master --depth 1 https://github.com/lisaac/luci-app-diskman diskman
 git clone -b master --depth 1 https://github.com/lisaac/luci-lib-docker docker_lib
 git clone -b master --depth 1 https://github.com/QiuSimons/openwrt-mos mosdns
 git clone -b master --depth 1 https://github.com/fw876/helloworld ssrp
 git clone -b master --depth 1 https://github.com/zxlhhyccc/bf-package-master zxlhhyccc
+git clone -b main --depth 1 https://github.com/linkease/openwrt-app-actions linkease
 #git clone -b linksys-ea6350v3-mastertrack --depth 1 https://github.com/NoTengoBattery/openwrt NoTengoBattery
 
 exit 0
